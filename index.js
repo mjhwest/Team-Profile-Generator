ea49600835140bb3fd6e709e32d4c231d638ac31
mjhwest89 <mjhwest89@gmail.com>
2021-11-09 18:25:36 +1030

    added notes on what to do

---
+++ index.js
@@ -0,0 +1,7 @@
+/* this is where the will actually run */
+
+//for code to run it requires each of the classes
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
