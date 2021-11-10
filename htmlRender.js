09f0f5805526bedea82f518e10a309f6f7125a2c
mjhwest89 <mjhwest89@gmail.com>
2021-11-10 18:55:22 +1030

    commit for saving all changes

---
+++ htmlRender.js
@@ -0,0 +1,6 @@
+function generateHTML()
+
+return
+
+//module.export is an object we use to store variable or a method
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* this is where the will actually run */
 const inquirer = require("inquirer");
-
+const fs = require('fs');
+const htmlRender = require("./htmlRender.js")
 
 //for code to run it requires each of the classes
 const Employee = require("./lib/employee");
