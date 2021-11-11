8dbf3e7edd509d6cdfdc82fd45b8a95cfad9939e
mjhwest89 <mjhwest89@gmail.com>
2021-11-11 13:05:54 +1030

    got tests to run

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /* this is where the will actually run */
-const inquirer = require("inquirer");
-const fs = require('fs');
-const htmlRender = require("./htmlRender.js")
 
-//for code to run it requires each of the classes
-const Employee = require("./lib/employee");
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
